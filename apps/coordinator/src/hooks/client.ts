a3e36b978eab911d8c07d4a920f794690029c964
Maude <maude@caravan-electrum.local>
2026-03-15 03:08:17 +0000

    Revert "[workspace-electrum] feat: Add ELECTRUM to ClientType union, enable Electrum backend selection"
    This reverts commit 090af2396348d7759ce7ffa039e20c3271c1d106.

--- apps/coordinator/src/hooks/client.ts
+++ apps/coordinator/src/hooks/client.ts
@@ -20,8 +20,6 @@ export const useGetClient = (): BlockchainClient => {
     client.username,
     client.password,
     client.walletName,
-    client.electrumBackendUrl,
-    client.electrumAuthToken,
     network,
   ]);
   return blockchainClient;
